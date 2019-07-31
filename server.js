458e554019c8dc3a607ac5890b04e50488f3d792
Brett <brett84c@gmail.com>
2019-07-31 18:36:59 -0400

    added path module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const path = require('path');
 const LinvoDB = require("linvodb3");
 LinvoDB.dbPath = `${process.cwd()}/vas-db`; 
 
