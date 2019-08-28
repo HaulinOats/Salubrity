4cdc598ee0c768b99824094fa65082ae1b2539ad
Brett <brett84c@gmail.com>
2019-08-27 22:05:46 -0400

    custom job header fix

--- client/src/Home/Home.js
+++ client/src/Home/Home.js
@@ -838,7 +838,7 @@ export default class Home extends Component{
                         <div key={item._id} className={'vas-home-table-tr vas-status-' + item.status + (item.openBy ? ' vas-home-table-row-is-open' : '')} onClick={(e)=>{this.selectJob(item)}}>
                           <div className='vas-home-table-tr-left vas-width-10'><Moment format='HH:mm'>{this.getDateFromObjectId(item._id)}</Moment></div>
                           <div className='vas-home-table-tr-right vas-width-90'>
-                            <p className='vas-home-table-job-name'>{item.job}{item.job === 'Custom' && ' - ' + item.customJob}</p>
+                            <p className='vas-home-table-job-name'>{item.job}{item.customJob ? ' - ' + item.customJob : ''}</p>
                             <div className='vas-home-table-tr-inner'>
                               <p><b>Room:</b><i className='vas-uppercase'>{item.room}</i></p>
                               <p><b>Hospital:</b><i className='vas-capitalize'>{this.state.hospitalsById[item.hospital].name ? this.state.hospitalsById[item.hospital].name : 'N/A'}</i></p>
